05827b1422a245eac53c748726e1985aa87d5e3e
Luana <luanadez13@gmail.com>
2021-03-27 13:44:11 -0300

    feat: login template

--- GitHub_app/src/app/app-routing.module.ts
+++ GitHub_app/src/app/app-routing.module.ts
@@ -7,15 +7,11 @@ const routes: Routes = [
     loadChildren: () => import('./main/main.module').then( m => m.MainPageModule)
   },
   {
-    path: 'github',
-    loadChildren: () => import('./main/main.module').then( m => m.MainPageModule)
-  },
-  {
     path: 'cadastro',
     loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
   },
   {
-    path: 'login',
+    path: '',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 
