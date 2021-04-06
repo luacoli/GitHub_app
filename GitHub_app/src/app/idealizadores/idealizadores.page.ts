ab1f88fa575f691c931ad9232cfa40af2983ac7f
LosAmigosTI <guilhermecamposdebrito@gmail.com>
2021-04-06 20:16:34 -0300

    Idealizadores prontos

--- GitHub_app/src/app/idealizadores/idealizadores.page.ts
+++ GitHub_app/src/app/idealizadores/idealizadores.page.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./idealizadores.page.scss'],
 })
 export class IdealizadoresPage implements OnInit {
+  digital: any;
 
   constructor() { }
 
